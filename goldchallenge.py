9e599fad9962b0bbe53ff923a019379e26e19f7e
AYMENE-K09 <khalidaymene09@gmail.com>
2025-09-15 10:35:17 +0100

    Update goldchallenge.py

--- goldchallenge.py
+++ goldchallenge.py
@@ -1,17 +1,15 @@
-print("enter your birthdate:")
-day = int(input("day: "))
-month = int(input("mont: "))
-year = int(input("year: "))
+print("enter your birthday DD/MM/YYYY ")
+Day = int(input("day: "))
+Month = int(input("month: "))
+Year = int(input("year: "))
 
-calcule = 2025 - year
-calcule_index = list(str(calcule))
-print(f"you are {calcule} years old")
-user_candles = int(calcule_index[1])
-c = ""
-for i in range(user_candles):
-    c += "i"
+user_age = 2025 - Year
+print(f"You were born in {Day}/{Month}/{Year} and you are {user_age} years old")
 
-print("    ___",c,"___")
+str_age = list(str(user_age))
+int_age = int(str_age[1])
+user_candles_numbers = int_age * "i"
+print("    ___",user_candles_numbers,"___")
 print("   |:H:a:p:p:y:|")
 print(" __|___________|__")
 print("|^^^^^^^^^^^^^^^^^|")
